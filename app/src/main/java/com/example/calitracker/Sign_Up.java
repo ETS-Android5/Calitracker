aaf3ab3364205d1f3241e0a23ced071e6426c83d
Cuyer <krystian123054@o2.pl>
2022-04-22 21:32:42 +0200

    no comment

--- app/src/main/java/com/example/calitracker/Sign_Up.java
+++ app/src/main/java/com/example/calitracker/Sign_Up.java
@@ -1,8 +1,11 @@
 package com.example.calitracker;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.calitracker.databinding.ActivitySignUpBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -26,6 +30,7 @@ private FirebaseAuth auth;
 
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,12 +44,75 @@ private FirebaseAuth auth;
         SignUpLastName = findViewById(R.id.last_name_editbox);
         SignUpDateOfBirth = findViewById(R.id.date_editbox);
 
+        SignUpMail.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SignUpMail.setBackgroundResource(R.drawable.border_black);
+                SignUpName.setBackgroundResource(R.drawable.border);
+                SignUpLastName.setBackgroundResource(R.drawable.border);
+                SignUpPass.setBackgroundResource(R.drawable.border);
+                SignUpDateOfBirth.setBackgroundResource(R.drawable.border);
+
+                return false;
+            }
+        });
+
+        SignUpPass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SignUpMail.setBackgroundResource(R.drawable.border);
+                SignUpName.setBackgroundResource(R.drawable.border);
+                SignUpLastName.setBackgroundResource(R.drawable.border);
+                SignUpPass.setBackgroundResource(R.drawable.border_black);
+                SignUpDateOfBirth.setBackgroundResource(R.drawable.border);
+                return false;
+            }
+        });
+
+        SignUpName.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SignUpMail.setBackgroundResource(R.drawable.border);
+                SignUpName.setBackgroundResource(R.drawable.border_black);
+                SignUpLastName.setBackgroundResource(R.drawable.border);
+                SignUpPass.setBackgroundResource(R.drawable.border);
+                SignUpDateOfBirth.setBackgroundResource(R.drawable.border);
+                return false;
+            }
+        });
+
+        SignUpLastName.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SignUpMail.setBackgroundResource(R.drawable.border);
+                SignUpName.setBackgroundResource(R.drawable.border);
+                SignUpLastName.setBackgroundResource(R.drawable.border_black);
+                SignUpPass.setBackgroundResource(R.drawable.border);
+                SignUpDateOfBirth.setBackgroundResource(R.drawable.border);
+                return false;
+            }
+        });
+
+        SignUpDateOfBirth.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SignUpMail.setBackgroundResource(R.drawable.border);
+                SignUpName.setBackgroundResource(R.drawable.border);
+                SignUpLastName.setBackgroundResource(R.drawable.border);
+                SignUpPass.setBackgroundResource(R.drawable.border);
+                SignUpDateOfBirth.setBackgroundResource(R.drawable.border_black);
+                return false;
+            }
+        });
+
+
 
         SignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
 
             // After clicking a "create account" button, it creates a new user.
             public void onClick(View view) {
+
                 String email = SignUpMail.getText().toString();
                 String pass = SignUpPass.getText().toString();
                 String name = SignUpName.getText().toString();
@@ -88,7 +156,15 @@ private FirebaseAuth auth;
                             });
                 }
             }
+
+
+
+
+
+
         });
+
+
 
 
 
