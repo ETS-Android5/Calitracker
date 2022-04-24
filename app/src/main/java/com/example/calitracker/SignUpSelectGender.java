83813f434cd3a777361af145acdd053efd931b62
Cuyer <krystian123054@o2.pl>
2022-04-24 17:55:55 +0200

    no comment

--- app/src/main/java/com/example/calitracker/SignUpSelectGender.java
+++ app/src/main/java/com/example/calitracker/SignUpSelectGender.java
@@ -5,11 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.calitracker.Model.EmailAndPass;
@@ -22,8 +21,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+
 public class SignUpSelectGender extends AppCompatActivity {
     private FirebaseAuth auth;
+    private boolean male_clicked = false;
+    private boolean female_clicked = false;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,42 +44,28 @@ public class SignUpSelectGender extends AppCompatActivity {
         femaleTextView = findViewById(R.id.female_textview);
         maleTextView = findViewById(R.id.male_textview);
 
-        femaleTextView.setOnTouchListener((view, motionEvent) -> {
-            femaleTextView.setBackgroundResource(R.drawable.border_black);
-            maleTextView.setBackgroundResource(R.drawable.border);
-            return false;
-        });
-
-        maleTextView.setOnTouchListener((view, motionEvent) -> {
-            maleTextView.setBackgroundResource(R.drawable.border_black);
-            femaleTextView.setBackgroundResource(R.drawable.border);
-            return false;
-        });
 
         femaleTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-            //TODO update plci jest zrobiony na kliknieciu w przycisk, a powinno byc po nacisnieciu next
-                //TODO mozna to zrobic poprzez boolean https://stackoverflow.com/questions/30661425/checking-if-a-button-has-been-clicked
-                Map<String, Object> gender = new HashMap<>();
-                gender.put("Gender", "Female");
-
-                auth.signInWithEmailAndPassword(EmailAndPass.email,EmailAndPass.pass);
-                FirebaseUser user = auth.getCurrentUser();
-                DocumentReference genderRef = db.collection("users").
-                        document(user.getUid());
-
-                genderRef.update(gender);
-
+                femaleTextView.setBackgroundResource(R.drawable.border_black);
+                maleTextView.setBackgroundResource(R.drawable.border);
+                female_clicked = true;
+                male_clicked = false;
             }
         });
 
+        maleTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                maleTextView.setBackgroundResource(R.drawable.border_black);
+                femaleTextView.setBackgroundResource(R.drawable.border);
+                male_clicked = true;
+                female_clicked = false;
 
 
-
-
+            }
+        });
 
         preferNotToSay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,14 +79,37 @@ public class SignUpSelectGender extends AppCompatActivity {
         nextTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(male_clicked){
+                    Map<String, Object> gender = new HashMap<>();
+                    gender.put("Gender", "Male");
+
+                    auth.signInWithEmailAndPassword(EmailAndPass.email,EmailAndPass.pass);
+                    FirebaseUser user = auth.getCurrentUser();
+                    DocumentReference genderRef = db.collection("users").
+                            document(user.getUid());
+
+                    genderRef.update(gender);
+                }
+
+                if(female_clicked){
+                    Map<String, Object> gender = new HashMap<>();
+                    gender.put("Gender", "Female");
+
+                    auth.signInWithEmailAndPassword(EmailAndPass.email,EmailAndPass.pass);
+                    FirebaseUser user = auth.getCurrentUser();
+                    DocumentReference genderRef = db.collection("users").
+                            document(user.getUid());
+
+                    genderRef.update(gender);
+                }
+
+
                 startActivity(new Intent(SignUpSelectGender.this,
                         SignUpSelectMetric.class));
                 overridePendingTransition(R.anim.slide_in, R.anim.slide_in);
             }
         });
-
-
-
 
 
     }
