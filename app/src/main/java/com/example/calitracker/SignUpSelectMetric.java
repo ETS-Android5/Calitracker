1afe4a205e53f3947c0ab57c5bc0d76fe63cfc33
Cuyer <krystian123054@o2.pl>
2022-04-24 18:30:11 +0200

    no comment

--- app/src/main/java/com/example/calitracker/SignUpSelectMetric.java
+++ app/src/main/java/com/example/calitracker/SignUpSelectMetric.java
@@ -3,11 +3,17 @@ package com.example.calitracker;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class SignUpSelectMetric extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_select_metric);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+
     }
 }
