5e812fa2bed0f3279347c499de1d55953ba2c1c4
Cuyer <krystian123054@o2.pl>
2022-04-30 12:23:58 +0200

    no comment

---
+++ app/src/main/java/com/example/calitracker/Controller/recyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.calitracker.Controller;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.calitracker.R;
+
+public class recyclerViewAdapter extends RecyclerView.Adapter<recyclerViewAdapter.ViewHolder> {
+
+    String[] data1, data2;
+    int[] images;
+    Context context;
+
+
+    public recyclerViewAdapter(Context ct, String[] s1, String[] s2, int[] img){
+    context = ct;
+    data1 = s1;
+    data2 = s2;
+    images = img;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1;
+        ImageView myImage;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.exerciseTextView);
+            myImage = itemView.findViewById(R.id.exerciseImageView);
+        }
+    }
+}
--- app/src/main/java/com/example/calitracker/HomeFragment.java
+++ app/src/main/java/com/example/calitracker/HomeFragment.java
@@ -3,18 +3,45 @@ package com.example.calitracker;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.calitracker.Controller.recyclerViewAdapter;
+
 
 public class HomeFragment extends Fragment {
+
+    String[] s1, s2;
+    int[] images = {R.drawable.squat,R.drawable.pull_up,R.drawable.handstand,R.drawable.leg_raises,
+    R.drawable.push_up,R.drawable.dips,R.drawable.horizontal_pull,R.drawable.plank};
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.exercises);
+        s2 = getResources().getStringArray(R.array.description);
+
+        recyclerViewAdapter recyclerViewAdapter = new recyclerViewAdapter(getActivity(),
+                s1,s2,images);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
