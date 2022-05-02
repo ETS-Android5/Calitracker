9eb95ed64add946989ef84d554cd6546db7b801b
Cuyer <krystian123054@o2.pl>
2022-05-02 12:19:51 +0200

    no comment

--- app/src/main/java/com/example/calitracker/Controller/ExercisesRecyclerView.java
+++ app/src/main/java/com/example/calitracker/Controller/ExercisesRecyclerView.java
@@ -1,5 +1,6 @@
 package com.example.calitracker.Controller;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,18 +12,21 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.calitracker.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class ExercisesRecyclerView extends RecyclerView.Adapter<ExercisesRecyclerView.MyViewHolder>
 {
-    String[] data1, data2;
+    String[] data1, data2, videoURL;
     int[] images;
     Context context;
 
-        public ExercisesRecyclerView(Context ct, String[] s1, String[] s2, int[] img) {
+        public ExercisesRecyclerView(Context ct, String[] s1, String[] s2, String[] videoArray) {
             context = ct;
             data1 = s1;
             data2 = s2;
-            images = img;
+            videoURL = videoArray;
         }
 
     @NonNull
@@ -37,6 +41,33 @@ public class ExercisesRecyclerView extends RecyclerView.Adapter<ExercisesRecycle
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.level.setText(data1[position]);
         holder.description.setText(data2[position]);
+        holder.playVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                YouTubePlayerView youTubePlayerView;
+                AlertDialog.Builder dialogBuilder;
+                AlertDialog dialog;
+                dialogBuilder = new AlertDialog.Builder(context);
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View view2 = inflater.inflate(R.layout.popup,null);
+                youTubePlayerView = view2.findViewById(R.id.youtube_player_view);
+                youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+                    @Override
+                    public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                        String videoID = videoURL[position];
+                        youTubePlayer.loadVideo(videoID,0);
+                        super.onReady(youTubePlayer);
+                    }
+                });
+
+
+
+                dialogBuilder.setView(view2);
+                dialog = dialogBuilder.create();
+                dialog.show();
+
+            }
+        });
 
     }
 
@@ -51,12 +82,14 @@ public class ExercisesRecyclerView extends RecyclerView.Adapter<ExercisesRecycle
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
             TextView level, description;
+            ImageView playVideoButton;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             level = itemView.findViewById(R.id.exercisesLevelTextView);
             description = itemView.findViewById(R.id.exercisesNameTextView);
+            playVideoButton = itemView.findViewById(R.id.playVideoButton);
 
         }
     }
--- app/src/main/java/com/example/calitracker/exercises.java
+++ app/src/main/java/com/example/calitracker/exercises.java
@@ -19,7 +19,7 @@ public class exercises extends AppCompatActivity {
 
     String data1;
     RecyclerView recyclerView;
-    String[] s1,s2;
+    String[] s1,s2,s3;
     int[] images = {R.drawable.squat,R.drawable.pull_up,R.drawable.handstand,R.drawable.leg_raises,
             R.drawable.push_up,R.drawable.dips,R.drawable.horizontal_pull,R.drawable.plank};
     ImageView goBackArrow;
@@ -39,53 +39,65 @@ public class exercises extends AppCompatActivity {
         if(data1.equals("SQUATS")) {
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.SQUATS);
+            s3 = getResources().getStringArray(R.array.SQUATS_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         } else if(data1.equals("PULL UPS")){
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.PULL_UPS);
+            s3 = getResources().getStringArray(R.array.PULL_UPS_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }else if(data1.equals("HANDSTAND PUSH UPS")){
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.HANDSTAND);
+            s3 = getResources().getStringArray(R.array.HANDSTAND_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }else if(data1.equals("LEG RAISES")){
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.LEGRAISES);
+            s3 = getResources().getStringArray(R.array.LEGRAISES_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }else if(data1.equals("PUSH UPS")){
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.PUSH_UPS);
+            s3 = getResources().getStringArray(R.array.PUSH_UPS_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }else if(data1.equals("DIPS")){
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.DIPS);
+            s3 = getResources().getStringArray(R.array.DIPS_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }else if(data1.equals("HORIZONTAL PULLS")){
             s1 = getResources().getStringArray(R.array.LEVEL);
             s2 = getResources().getStringArray(R.array.HORIZONTAL_PULLS);
+            s3 = getResources().getStringArray(R.array.HORIZONTAL_PULLS_VIDEOURL);
             ExercisesRecyclerView exercisesRecyclerView = new ExercisesRecyclerView(this,
-                    s1,s2,images);
+                    s1,s2,s3);
             recyclerView.setAdapter(exercisesRecyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }
+
+
+
+
+
         goBackArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
