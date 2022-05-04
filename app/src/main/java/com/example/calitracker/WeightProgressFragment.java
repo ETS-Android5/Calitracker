7a232276220b602043e8c9521844f8bc037317dc
Cuyer <krystian123054@o2.pl>
2022-05-04 16:21:52 +0200

    no comment

--- app/src/main/java/com/example/calitracker/WeightProgressFragment.java
+++ app/src/main/java/com/example/calitracker/WeightProgressFragment.java
@@ -1,21 +1,25 @@
 package com.example.calitracker;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -23,16 +27,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-
+import com.example.calitracker.Controller.TextViewDrawer;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.w3c.dom.Text;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class WeightProgressFragment extends Fragment {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference("chartTable");
+    LineGraphSeries lineGraphSeries;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+    final Calendar myCalendar= Calendar.getInstance();
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,6 +76,29 @@ public class WeightProgressFragment extends Fragment {
        TextView textView = new TextView(getActivity());
        LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.linearLayoutWeightProgress);
 
+        GraphView graphView = (GraphView)view.findViewById(R.id.graph);
+        lineGraphSeries = new LineGraphSeries();
+        graphView.addSeries(lineGraphSeries);
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if(isValueX){
+                    return sdf.format(new Date((long) value));
+                }else{
+                    return super.formatLabel(value, isValueX);
+                }
+
+            }
+        });
+        graphView.getGridLabelRenderer().setNumHorizontalLabels(3);
+        graphView.getGridLabelRenderer().setNumVerticalLabels(3);
+        graphView.getViewport().setBackgroundColor(Color.WHITE);
+        graphView.getGridLabelRenderer().setGridColor(Color.TRANSPARENT);
+        graphView.getViewport().setDrawBorder(true);
+        graphView.getViewport().setBorderColor(Color.BLACK);
+        graphView.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+        graphView.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+
 
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +108,9 @@ public class WeightProgressFragment extends Fragment {
                 showDialog();
 
             }
+
+
+
 
 
             private TextView createNewTextView(String weight, String date){
@@ -76,6 +128,10 @@ public class WeightProgressFragment extends Fragment {
             }
 
 
+
+
+
+
             private void showDialog() {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),
                         R.style.AlertDialogTheme);
@@ -88,6 +144,33 @@ public class WeightProgressFragment extends Fragment {
                        .findViewById(R.id.WeightEditTextNumberDecimal);
                final EditText dateEditText = (EditText)view
                        .findViewById(R.id.WeightDateEditTextDate);
+
+
+                DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, month);
+                        myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                        dateEditText.setText(sdf.format(myCalendar.getTime()));
+                    }
+                };
+
+
+               dateEditText.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+
+                       new DatePickerDialog(getActivity(),date,myCalendar.get(Calendar.YEAR),
+                               myCalendar.get(Calendar.MONTH),myCalendar
+                               .get(Calendar.DAY_OF_MONTH)).show();
+                   }
+               });
+
+
+
+
+
 
                 builder.setPositiveButton("ADD",
                         new DialogInterface.OnClickListener() {
@@ -102,6 +185,17 @@ public class WeightProgressFragment extends Fragment {
                             } else{
                                 linearLayout.addView(createNewTextView(weightEditText.getText().toString(),
                                         dateEditText.getText().toString()));
+                                String id = reference.push().getKey();
+
+                                try {
+                                    Date date = sdf.parse(dateEditText.getText().toString());
+                                    long x = date.getTime();
+                                    int y = Integer.parseInt(weightEditText.getText().toString());
+                                    PointValue pointValue = new PointValue(x,y);
+                                    reference.child(id).setValue(pointValue);
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
 
                             }
 
@@ -113,10 +207,85 @@ public class WeightProgressFragment extends Fragment {
                 builder.show();
             }
 
+
+
+
         });
 
 
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int index = 0;
+                DataPoint[] dp = new DataPoint[(int) snapshot.getChildrenCount()];
+                for(DataSnapshot myDataSnapshot : snapshot.getChildren()){
+                    PointValue pointValue = myDataSnapshot.getValue(PointValue.class);
+                    dp[index] = new DataPoint(pointValue.getxValue(), pointValue.getyValue());
+                    String str1 = Integer.toString(pointValue.getyValue());
+                    String str2 = sdf.format(new Date(pointValue.getxValue()));
+                    linearLayout.addView(createTextView(str1,str2));
+                    index ++;
+                }
+
+            }
+
+            private TextView createTextView(String weight, String date) {
+                final LinearLayout.LayoutParams layoutParams = new LinearLayout
+                        .LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                TextView textView1 = new TextView(getActivity());
+                textView1.setLayoutParams(layoutParams);
+                textView1.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+                Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.roboto);
+                textView1.setText(weight + " " + date);
+                return textView1;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
         return view;
+    }
+
+    @Override
+    public void onStart() {
+
+
+
+
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DataPoint[] dp = new DataPoint[(int) snapshot.getChildrenCount()];
+                int index = 0;
+                for(DataSnapshot myDataSnapshot : snapshot.getChildren()){
+                    PointValue pointValue = myDataSnapshot.getValue(PointValue.class);
+                    dp[index] = new DataPoint(pointValue.getxValue(), pointValue.getyValue());
+                    index ++;
+                }
+                lineGraphSeries.resetData(dp);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        super.onStart();
+
     }
 
 }
