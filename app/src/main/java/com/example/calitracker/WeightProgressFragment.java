6c1f7b6a21c45fef4abe577295d54b976e834451
Cuyer <krystian123054@o2.pl>
2022-05-06 06:48:35 +0200

    no comment

--- app/src/main/java/com/example/calitracker/WeightProgressFragment.java
+++ app/src/main/java/com/example/calitracker/WeightProgressFragment.java
@@ -13,21 +13,19 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-import com.example.calitracker.Controller.TextViewDrawer;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -42,16 +40,15 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import org.w3c.dom.Text;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 public class WeightProgressFragment extends Fragment {
 
@@ -59,9 +56,8 @@ public class WeightProgressFragment extends Fragment {
     DatabaseReference reference = database.getReference("users");
     LineGraphSeries lineGraphSeries;
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
-    final Calendar myCalendar= Calendar.getInstance();
+    final Calendar myCalendar = Calendar.getInstance();
     FirebaseAuth auth;
-
 
 
     @Override
@@ -69,7 +65,7 @@ public class WeightProgressFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         auth = FirebaseAuth.getInstance();
-                FirebaseUser user = auth.getCurrentUser();
+        FirebaseUser user = auth.getCurrentUser();
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_weight_progress, container,
@@ -78,18 +74,18 @@ public class WeightProgressFragment extends Fragment {
 
         FloatingActionButton floatingActionButton = (FloatingActionButton) view
                 .findViewById(R.id.add_weight_floatingActionButton);
-       TextView textView = new TextView(getActivity());
-       LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.linearLayoutWeightProgress);
+        TextView textView = new TextView(getActivity());
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.linearLayoutWeightProgress);
 
-        GraphView graphView = (GraphView)view.findViewById(R.id.graph);
+        GraphView graphView = (GraphView) view.findViewById(R.id.graph);
         lineGraphSeries = new LineGraphSeries();
         graphView.addSeries(lineGraphSeries);
-        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
-                if(isValueX){
+                if (isValueX) {
                     return sdf.format(new Date((long) value));
-                }else{
+                } else {
                     return super.formatLabel(value, isValueX);
                 }
 
@@ -105,7 +101,6 @@ public class WeightProgressFragment extends Fragment {
         graphView.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
 
 
-
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -115,10 +110,7 @@ public class WeightProgressFragment extends Fragment {
             }
 
 
-
-
-
-            private TextView createNewTextView(String weight, String date){
+            private TextView createNewTextView(String weight, String date) {
                 final LinearLayout.LayoutParams layoutParams = new LinearLayout
                         .LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -133,10 +125,6 @@ public class WeightProgressFragment extends Fragment {
             }
 
 
-
-
-
-
             private void showDialog() {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),
                         R.style.AlertDialogTheme);
@@ -145,10 +133,10 @@ public class WeightProgressFragment extends Fragment {
                 final View view = inflater.inflate(R.layout.add_weight_layout,
                         null, false);
 
-               final EditText weightEditText = (EditText)view
-                       .findViewById(R.id.WeightEditTextNumberDecimal);
-               final EditText dateEditText = (EditText)view
-                       .findViewById(R.id.WeightDateEditTextDate);
+                final EditText weightEditText = (EditText) view
+                        .findViewById(R.id.WeightEditTextNumberDecimal);
+                final EditText dateEditText = (EditText) view
+                        .findViewById(R.id.WeightDateEditTextDate);
 
 
                 DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -156,82 +144,82 @@ public class WeightProgressFragment extends Fragment {
                     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                         myCalendar.set(Calendar.YEAR, year);
                         myCalendar.set(Calendar.MONTH, month);
-                        myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, day);
                         dateEditText.setText(sdf.format(myCalendar.getTime()));
                     }
                 };
 
 
-               dateEditText.setOnClickListener(new View.OnClickListener() {
-                   @Override
-                   public void onClick(View view) {
+                dateEditText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
-                       new DatePickerDialog(getActivity(),date,myCalendar.get(Calendar.YEAR),
-                               myCalendar.get(Calendar.MONTH),myCalendar
-                               .get(Calendar.DAY_OF_MONTH)).show();
-                   }
-               });
-
+                        new DatePickerDialog(getActivity(), date, myCalendar.get(Calendar.YEAR),
+                                myCalendar.get(Calendar.MONTH), myCalendar
+                                .get(Calendar.DAY_OF_MONTH)).show();
+                    }
+                });
 
 
                 builder.setPositiveButton("ADD",
                         new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                            if(TextUtils.isEmpty(weightEditText.getText().toString()) || TextUtils
-                                    .isEmpty(dateEditText.getText().toString())) {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if (TextUtils.isEmpty(weightEditText.getText().toString()) || TextUtils
+                                        .isEmpty(dateEditText.getText().toString())) {
 
-                                Toast.makeText(getActivity(),"Please provide weight and date",
-                                        Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getActivity(), "Please provide weight and date",
+                                            Toast.LENGTH_SHORT).show();
 
-                            } else{
-                                linearLayout.addView(createNewTextView(weightEditText.getText().toString(),
-                                        dateEditText.getText().toString()));
+                                } else {
+                                    linearLayout.addView(createNewTextView(weightEditText.getText().toString(),
+                                            dateEditText.getText().toString()));
 
-                                String id = user.getUid();
-                                String randomId = reference.push().getKey();
+                                    String id = user.getUid();
+                                    String randomId = reference.push().getKey();
 
-                                try {
-                                    Date date = sdf.parse(dateEditText.getText().toString());
-                                    long x = date.getTime();
-                                    int y = Integer.parseInt(weightEditText.getText().toString());
-                                    PointValue pointValue = new PointValue(x,y);
-                                    reference.child(id).child(randomId).setValue(pointValue);
-                                } catch (ParseException e) {
-                                    e.printStackTrace();
+                                    try {
+                                        Date date = sdf.parse(dateEditText.getText().toString());
+                                        long x = date.getTime();
+                                        int y = Integer.parseInt(weightEditText.getText().toString());
+                                        PointValue pointValue = new PointValue(x, y);
+                                        reference.child(id).child(randomId).setValue(pointValue);
+
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+
                                 }
 
                             }
 
-                    }
-
-                });
+                        });
                 builder.setView(view);
 
                 builder.show();
             }
 
 
-
-
         });
 
+        String id = user.getUid();
+        Query dateAscendingOrder = database.getReference("users")
+                .child(id).orderByChild("xValue");
 
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+        dateAscendingOrder.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String id = user.getUid();
                 int index = 0;
 
-                DataPoint[] dp = new DataPoint[(int) snapshot.child(id).getChildrenCount()];
-                for(DataSnapshot myDataSnapshot : snapshot.child(id).getChildren()){
+                DataPoint[] dp = new DataPoint[(int) snapshot.getChildrenCount()];
+                for (DataSnapshot myDataSnapshot : snapshot.getChildren()) {
                     PointValue pointValue = myDataSnapshot.getValue(PointValue.class);
                     dp[index] = new DataPoint(pointValue.getxValue(), pointValue.getyValue());
                     String str1 = Integer.toString(pointValue.getyValue());
                     String str2 = sdf.format(new Date(pointValue.getxValue()));
-                    linearLayout.addView(createTextView(str1,str2));
-                    index ++;
+                    linearLayout.addView(createTextView(str1, str2));
+                    index++;
                 }
 
             }
@@ -256,37 +244,34 @@ public class WeightProgressFragment extends Fragment {
         });
 
 
-
-
         return view;
     }
 
     @Override
     public void onStart() {
+        FirebaseAuth auth;
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        String id = user.getUid();
 
+        Query dateAscendingOrder = database.getReference("users")
+                .child(id).orderByChild("xValue");
 
-
-
-
-
-        reference.addValueEventListener(new ValueEventListener() {
+        dateAscendingOrder.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                FirebaseAuth auth;
-                auth = FirebaseAuth.getInstance();
-                FirebaseUser user = auth.getCurrentUser();
-                String id = user.getUid();
-                DataPoint[] dp = new DataPoint[(int) snapshot.child(id).getChildrenCount()];
+
+                DataPoint[] dp = new DataPoint[(int) snapshot.getChildrenCount()];
                 int index = 0;
-                for(DataSnapshot myDataSnapshot : snapshot.child(id).getChildren()){
+                for (DataSnapshot myDataSnapshot : snapshot.getChildren()){
+
                     PointValue pointValue = myDataSnapshot.getValue(PointValue.class);
                     dp[index] = new DataPoint(pointValue.getxValue(), pointValue.getyValue());
-                    index ++;
+                    index++;
                 }
 
+
                 lineGraphSeries.resetData(dp);
-
-
             }
 
             @Override
@@ -294,6 +279,7 @@ public class WeightProgressFragment extends Fragment {
 
             }
         });
+
         super.onStart();
 
     }
